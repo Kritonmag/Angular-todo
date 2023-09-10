059693f9c10a37fe87f4e7ca2039187752b3c055
Vyacheslav <kriton.mag@gmail.com>
2023-09-10 15:49:15 +0300

    don`t use material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  editingTaskIndex: number | null = null;
+  editedTaskText: string = '';
+
+  tasks = [
+    { text: 'Купить продукты', completed: false },
+    { text: 'Сделать todo', completed: false },
+  ];
+
+  markAsCompleted(task: any): void {
+    task.completed = !task.completed;
+  }
+
+  deleteTask(task: any): void {
+    const index = this.tasks.indexOf(task);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+    }
+  }
+
+  startEditing(index: number): void {
+    this.editingTaskIndex = index;
+    this.editedTaskText = this.tasks[index].text;
+  }
+
+  saveEditedTask(index: number): void {
+    this.tasks[index].text = this.editedTaskText;
+    this.editingTaskIndex = null;
+  }
+
+  cancelEditing(): void {
+    this.editingTaskIndex = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatCardModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
