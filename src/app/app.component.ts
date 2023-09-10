ec04a00bed3c30f7bcd4fcb14ab058a5aa07d94e
Vyacheslav <kriton.mag@gmail.com>
2023-09-10 17:45:47 +0300

    use material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,43 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  title: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'todo';
 
   editingTaskIndex: number | null = null;
   editedTaskText: string = '';
+  newTask = '';
 
-  tasks = [
-    { text: 'Купить продукты', completed: false },
-    { text: 'Сделать todo', completed: false },
+  tasks: Task[] = [
+    {
+      title: 'Купить хлеб, воду, печенье',
+      completed: false
+    },
+    {
+      title: 'Помыть посуду, пылесосить',
+      completed: false
+    },
+    {
+      title: 'Приготовить пасту с соусом и салат',
+      completed: false
+    }
   ];
 
-  markAsCompleted(task: any): void {
+  markAsCompleted(task: Task): void {
     task.completed = !task.completed;
   }
 
-  deleteTask(task: any): void {
+  deleteTask(task: Task): void {
     const index = this.tasks.indexOf(task);
     if (index !== -1) {
       this.tasks.splice(index, 1);
@@ -29,15 +46,26 @@ export class AppComponent {
 
   startEditing(index: number): void {
     this.editingTaskIndex = index;
-    this.editedTaskText = this.tasks[index].text;
+    this.editedTaskText = this.tasks[index].title;
   }
 
   saveEditedTask(index: number): void {
-    this.tasks[index].text = this.editedTaskText;
+    this.tasks[index].title = this.editedTaskText;
     this.editingTaskIndex = null;
   }
 
   cancelEditing(): void {
     this.editingTaskIndex = null;
+  }
+
+  addNewTask(): void {
+    if (this.newTask.trim() !== '') {
+      const newTaskObject = {
+        title: this.newTask,
+        completed: false
+      };
+      this.tasks.unshift(newTaskObject);
+      this.newTask = '';
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -21,6 +25,10 @@ import { FormsModule } from '@angular/forms';
     MatListModule,
     MatCardModule,
     FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
